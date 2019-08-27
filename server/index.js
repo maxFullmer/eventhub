b13e2aeedcc691684f1d156958393ece5c0eb195
Adam Kent <Adam@Adams-MacBook-Pro-2.local>
2019-08-27 09:23:07 -0700

    basic server

---
+++ server/index.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose")
+
+app.use(express.json());
+
+const { SERVER_PORT, CONNECTION_STRING } = process.env;
+
+// mongoose.connect(CONNECTION_STRING, { useNewUrlParser: true, useCreateIndex: true }).then(() => {
+//   console.log('connection successful');
+// })
+
+const port = SERVER_PORT ;
+app.listen(port, () => console.log(`server up and running on ${port}`));
