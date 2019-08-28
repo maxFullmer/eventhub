b63ecd1731a288bdbfb7073226b242c989274462
Max-F <max.fullmer@outlook.com>
2019-08-28 13:42:08 -0700

    map only displays

--- server/index.js
+++ server/index.js
@@ -5,11 +5,16 @@ const mongoose = require("mongoose")
 
 app.use(express.json());
 
-const { SERVER_PORT, CONNECTION_STRING } = process.env;
+const { SERVER_PORT, CONNECTION_STRING, GOOGLE_MAPS_API_KEY } = process.env;
 
 mongoose.connect(CONNECTION_STRING, { useNewUrlParser: true, useCreateIndex: true }).then(() => {
   console.log('connection successful');
 })
+
+//use the google maps api key to get geocode lat and lng?
+// app.get('/api/mapme', (req, res) => {
+  
+// }
 
 const port = SERVER_PORT ;
 app.listen(port, () => console.log(`server up and running on ${port}`));
--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import {Route, Switch} from 'react-router-dom';
-
+import { Route, Switch } from 'react-router-dom';
+import { withScriptjs, withGoogleMap } from 'react-google-maps'
 import Login from './Components/Login/Login';
 import Main from './Components/Main/Main';
 import Gmap from './Components/Gmap/Gmap';
 import User from './Components/User/User';
 import './App.css';
+// import { GOOGLE_MAPS_API_KEY } from '../.env';
+
+const WrappedMap = withScriptjs(withGoogleMap(Gmap));
+
+// console.log(GOOGLE_MAPS_API_KEY)
 
 function App() {
   return (
@@ -13,7 +18,17 @@ function App() {
       <Switch>
         <Route exact path="/" component={Login} />
         <Route path="/main" component={Main} />
-        <Route path="/map" component={Gmap} />
+        <Route path="/map" render={(props) => (
+          <div id="wrapped-map-container">
+            <WrappedMap 
+                // googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${GOOGLE_MAPS_API_KEY}`}
+                googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=places`}
+                loadingElement={<div style={{height: "100%"}}/>}
+                containerElement={<div style={{height: "100%"}}/>}
+                mapElement={<div style={{height: "100%"}}/>}
+            />
+          </div>
+        )} />
         <Route path="/user" component={User} />
       </Switch>
     </div>
