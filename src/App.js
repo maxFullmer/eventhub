885eab22cd919044d121da0a61e9fbf0964bb1a5
Brandon Caig <bcraig2612@gmail.com>
2019-08-28 23:37:50 -0700

    finished landing page

--- src/App.js
+++ src/App.js
@@ -5,17 +5,19 @@ import Main from './Components/Main/Main';
 import Map from './Components/Map/Map';
 import User from './Components/User/User';
 import RegisterForm from './Components/Login/Registration/RegisterForm';
+import Landing from './Components/Landing/Landing';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Switch>
-        <Route exact path="/" component={LoginForm} />
-        <Route path="/main" component={Main} />
-        <Route path="/map" component={Map} />
-        <Route path="/user" component={User} />
+        <Route exact path="/" component={Landing} />
+        <Route path="/login" component={LoginForm} />
         <Route path="/register" component={RegisterForm} />
+        <Route path="/main" component={Main} />
+        <Route path="/user" component={User} />
+        <Route path="/map" component={Map} />
       </Switch>
     </div>
   );
