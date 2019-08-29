d0de74ad5cdbda2757fee8eb1ce2e55af90e6920
Max-F <max.fullmer@outlook.com>
2019-08-29 11:05:23 -0700

    Gmap Jam

--- src/Components/Gmap/Gmap.js
+++ src/Components/Gmap/Gmap.js
@@ -4,52 +4,20 @@ import axios from 'axios';
 
 
 export default function Gmap() {
-    // let eventLocation = (eventObj) => {
-    //     axios.get('https://maps.googleapis.com/maps/api/geocode/json', {
-    //         params: {
-    //             address: `${eventObj.address.street} ${eventObj.address.city} ${eventObj.address.state} ${eventObj.address.zip}`,
-    //             key: GOOGLE_MAPS_API_KEY
-    //         }
-    //     })
-    //     .then(response => {
-    //         let lat = response.data.results[0].geometry.location.lat;
-    //         let lng = response.data.results[0].geometry.location.lng;
-    //     })
-    //     .catch(error => {
-    //         console.log(error)
-    //     })
+    let markers = filteredEvents.map(event => (
+        <Marker 
+            key={event.eventID}
+            position={{lat: event.lat, lng: event.lng}}
+        />
+    ))
 
-
-    // }
     return (
         <GoogleMap 
             defaultZoom={10} 
             defaultCenter={{lat: 36.921719, lng: -76.014900}}
-            // defaultCenter={{'city coords from state or props' eventLocation()}}
+            // defaultCenter={{'city coords from state or props' --> {lat: }}}
         >
-            {/* {filteredEvents.map(event => (
-                <Marker 
-                    key={event.eventID}
-                    position={{lat: eventLocation(event).lat, lng: eventLocation(event).lng}}
-                />
-            ))} */}
+            {markers}
         </GoogleMap>
     );
 }
-
-    // const WrappedMap = withScriptjs(withGoogleMap(gmap));
-
-//     return (
-//         <div id="wrapped-map-container">
-//             <WrappedMap 
-//                 googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.GOOGLE_MAPS_API_KEY}`}
-//                 loadingElement={<div style={{height: "100%"}}/>}
-//                 containerElement={<div style={{height: "100%"}}/>}
-//                 mapElement={<div style={{height: "100%"}}/>}
-//             />
-//         </div>
-//     )
-// }
-
-
-
