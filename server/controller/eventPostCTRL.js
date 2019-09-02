40d7233b0cd105022221f4bf3f9cab6d3fc261c3
Max-F <max.fullmer@outlook.com>
2019-09-02 14:10:47 -0700

    cleaned up dependencies

--- server/controller/eventPostCTRL.js
+++ server/controller/eventPostCTRL.js
@@ -8,15 +8,16 @@ const geocodio = new Geocodio(geoConfig);
 
 module.exports = {
     eventPost: (req, res, next) => {
-        const { address } = req.body;
-        // const address = `${streetAddress}, ${city}, ${state} ${zip}`;
+        const { formatted_address, city } = req.body;
       
-        geocodio.get('geocode', {q: address}, function(err, response){
+        //send city to 
+        console.log(city)
+        geocodio.get('geocode', {q: formatted_address}, function(err, response){
           if (err) throw err;
       
           const resStrToJSON = JSON.parse(response);
           const location = resStrToJSON.results[0].location;
-          // write MongoDB post-event method that adds the req.body address info and the lat-lng location 
+          // write MongoDB post-event method that adds the req.body formatted_address and city along with the lat-lng location 
           res.status(200).send(location);
           // res.status(200).send('do we need to send anything back??? maybe the array of event ids for the user?')
         });
