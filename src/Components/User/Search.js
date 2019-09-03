8ee168555232858e699c151963cb3e98f8148e17
Max-F <max.fullmer@outlook.com>
2019-09-03 10:00:23 -0700

    added js methods for form

--- src/Components/User/Search.js
+++ src/Components/User/Search.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 const google = window.google;
 
 class Search extends Component {
@@ -47,6 +48,20 @@ class Search extends Component {
         })
     }
 
+    handleSubmit = (event) => {
+        event.preventDefault();
+        const { formatted_address, city, description, event_name } = this.state;
+
+        axios.post('/api/post-event', {
+            formatted_address: formatted_address,
+            city: city,
+            description: description,
+            event_name: event_name
+        })
+        .then(response => console.log(response))
+        .catch(error => console.log(error));
+    }
+
     render() {
         console.log('city: ', this.state.city);
         console.log('formatted_address: ', this.state.formatted_address);
@@ -56,9 +71,67 @@ class Search extends Component {
                 <h6>Search Address:</h6>
                 <form onSubmit={this.handleSubmit}>
                     <input id="autocomplete" className="form-control" ref="input" type="text" />
+                    <button type="submit">submit</button>
                 </form>
             </div>
         );
     }
 }
 export default Search;
+
+// import React, { Component } from 'react';
+// const google = window.google;
+// class Search extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = this.initialState();
+//         this.handlePlaceSelect = this.handlePlaceSelect.bind(this);
+//         this.handleChange = this.handleChange.bind(this);
+//         this.autocomplete = null;
+//     }
+//     initialState() {
+//         return {
+//             address: {
+//                 street_number: '',
+//                 street_name: '',
+//                 city: '',
+//                 state: '',
+//                 zip: ''
+//             }
+//         }
+//     }
+//     componentDidMount() {
+//         this.autocomplete = new google.maps.places.Autocomplete(document.getElementById('autocomplete'), {})
+//         this.autocomplete.setFields(['address_components']);
+//         this.autocomplete.addListener("place_changed", this.handlePlaceSelect)
+//     }
+//     handleChange(event) {
+//         this.setState({ [event.target.name]: event.target.value })
+//     }
+//     handlePlaceSelect() {
+//         let addressObject = this.autocomplete.getPlace()
+//         console.log(addressObject);
+//         let address = addressObject.address_components
+//         this.setState({
+//             street_number: address.short_name,
+//             street_name: address.long_name,
+//             city: address.filter(element => {
+//                 return element.types.includes("locality")
+//             })[0].long_name,
+//             state: address.short_name,
+//             zip: address.short_name
+//         })
+//     }
+//     render() {
+//         console.log(this.state.city);
+//         return (
+//             <div className="form-group">
+//                 <h6>Search Address:</h6>
+//                 <form onSubmit={this.handleSubmit}>
+//                     <input id="autocomplete" className="form-control" ref="input" type="text" />
+//                 </form>
+//             </div>
+//         );
+//     }
+// }
+// export default Search;
