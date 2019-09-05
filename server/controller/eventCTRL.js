47082a6b1e23d06b256b41caaf162d7234d36316
Max-F <max.fullmer@outlook.com>
2019-09-05 10:40:49 -0700

    messed around with the delete endpoint

--- server/controller/eventCTRL.js
+++ server/controller/eventCTRL.js
@@ -62,6 +62,29 @@ module.exports = {
     
       res.sendStatus(200);
     });
+  },
+
+  cancelEvent: (req, res, next) => {
+    const { event_id, user_id } = req.body;
+
+
+    User.findOne({_id: mongoose.Types.ObjectId(user_id)}).then((foundUser) => {
+      let event_index = foundUser.userEvents.indexOf(mongoose.Types.ObjectId(event_id));
+      foundUser.userEvents.splice(event_index, 1);
+      foundUser.save((err) => {
+        if (err) throw err;
+      })
+    });
+         
+    try {
+      console.log(Event.deleteOne({_id: mongoose.Types.ObjectId(event_id)}))
+      Event.deleteOne({_id: mongoose.Types.ObjectId(event_id)});
+    } catch (err) {
+      console.log(err)
+    }
+    
+
+    res.sendStatus(200)
   }
 
   // editEvent: (req, res, next) => {
