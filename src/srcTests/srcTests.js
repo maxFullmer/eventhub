302981261e588a6a601afba221a6842ac83bc67f
Max-F <max.fullmer@outlook.com>
2019-09-10 20:14:08 -0400

    Minor cleanup and added tests

--- src/srcTests/srcTests.js
+++ src/srcTests/srcTests.js
@@ -1,16 +1,16 @@
-module.exports = {
-    //unit test functions (only one should be used in one unit test)
-    capitalizer(city) {
-        let capitalize = city.toLowerCase().split(' ')
-        .map((str) => str.charAt(0).toUpperCase() + str.substring(1))
-        .join(' ');
-        return capitalize
-    },
+// module.exports = {
+//     //unit test functions (only one should be used in one unit test)
+//     capitalizer(city) {
+//         let capitalize = city.toLowerCase().split(' ')
+//         .map((str) => str.charAt(0).toUpperCase() + str.substring(1))
+//         .join(' ');
+//         return capitalize
+//     },
 
-    loadingCheck(loadingState) {
-        return loadingState;
-    }
+//     loadingCheck(loadingState) {
+//         return loadingState;
+//     }
 
-    // integration tests functions (multiple should/will be used in one integration test)
+//     // integration tests functions (multiple should/will be used in one integration test)
     
-}
+// }
--- src/srcTests/srcTests.test.js
+++ src/srcTests/srcTests.test.js
@@ -1,44 +1,44 @@
-const srcTest = require('./srcTests')
+// const srcTest = require('./srcTests')
 
-// unit tests
-describe('unit tests', () => {
-    describe('capitalizer', () => {
-      it('should Capitalize city name', () => {
-        let city = "virginia beach";
+// // unit tests
+// describe('unit tests', () => {
+//     describe('capitalizer', () => {
+//       it('should Capitalize city name', () => {
+//         let city = "virginia beach";
   
-        expect(srcTest.capitalizer(city)).toEqual("Virginia Beach");
-      })
-    })
+//         expect(srcTest.capitalizer(city)).toEqual("Virginia Beach");
+//       })
+//     })
 
-    describe('loadingCheck', () => {
-        it('should verify that the value of loading (property on state obj) is a boolean', () => {
-            let loadingTrue = true;
-            let loadingFalse = false;
-            expect(srcTest.loadingCheck(loadingTrue)).toBe(true);
-            expect(srcTest.loadingCheck(loadingFalse)).toBe(false)
-        })
-    })
-})
+//     describe('loadingCheck', () => {
+//         it('should verify that the value of loading (property on state obj) is a boolean', () => {
+//             let loadingTrue = true;
+//             let loadingFalse = false;
+//             expect(srcTest.loadingCheck(loadingTrue)).toBe(true);
+//             expect(srcTest.loadingCheck(loadingFalse)).toBe(false)
+//         })
+//     })
+// })
 
-// integraton aka E2E tests
-describe('Integration aka E2E tests', () => {
-    describe('integratonTest1', () => {
-        describe('functionForIntegrationTestA', () => {
+// // integraton aka E2E tests
+// describe('Integration aka E2E tests', () => {
+//     describe('integratonTest1', () => {
+//         describe('functionForIntegrationTestA', () => {
             
-        })
+//         })
         
-        describe('functionForIntegrationTestB', () => {
+//         describe('functionForIntegrationTestB', () => {
 
-        })
-    })
+//         })
+//     })
 
-    describe('integratonTest2', () => {
-        describe('functionForIntegrationTestA', () => {
+//     describe('integratonTest2', () => {
+//         describe('functionForIntegrationTestA', () => {
             
-        })
+//         })
 
-        describe('functionForIntegrationTestB', () => {
+//         describe('functionForIntegrationTestB', () => {
 
-        })
-    })
-})
+//         })
+//     })
+// })
